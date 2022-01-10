75ab637c0208b7aa9236a77d3e194362af529d29
*WV56qweBnZ <duynd@midota.com>
2022-01-10 07:42:28 +0700

    update stt

--- resources/js/page/TestList.js
+++ resources/js/page/TestList.js
@@ -9,16 +9,21 @@ import { testListURL } from "../../constants/backend_url";
 function TestList({}) {
     const dispatch = useDispatch();
     const testList = useSelector((state) => state.testListReducers.testList);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
     useEffect(() => {
         dispatch(getListAction());
     }, [dispatch]);
     
     const [form] = useForm();
     const [isModalVisible, setIsModalVisible] = useState(false);
+
+    //handle show modal
     const showModal = () => {
         setIsModalVisible(true);
     };
-
+   
+    //handle cancel modal
     const handleCancel = () => {
         setIsModalVisible(false);
     };
@@ -26,13 +31,13 @@ function TestList({}) {
     const columns = [
         {
             title: "STT",
-            dataIndex: "id",
             key: "id",
             align: "center",
             width: 120,
-            // render: (index, record,stt) =>{
-            //     return <p>{stt+1}</p>
-            // }
+            render: (index, record,stt) =>{
+                var index =pageSize*(currentPage-1) + (stt+1)
+                return <p>{index}</p>
+            }
         },
         {
             title: "Tiêu đề",
@@ -66,10 +71,11 @@ function TestList({}) {
             align: "center",
         },
     ];
-    function onChange(){
-        console.log("xx")
+
+    //handle change when change pagination
+    function onChange(currentPageData){
+        setCurrentPage(currentPageData.current)
     }
-    console.log("TestList:",testList)
     return (
         <div className="content-page">
             <div className="title">Danh sách bài kiểm tra</div>
@@ -89,7 +95,7 @@ function TestList({}) {
                  handleCancel={handleCancel}
                  form={form}
             />
-            <Table  columns={columns} dataSource={testList}  onChange={onChange}/>
+            <Table  columns={columns} dataSource={testList}  onChange={(pagination, filters, sorter, currentPageData) =>onChange(pagination)}/>
         </div>
     );
 }
